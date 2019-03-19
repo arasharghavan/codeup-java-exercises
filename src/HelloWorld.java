1d28e9543753cdb3bdcbf46d1238d4434335b15b
Arash <arash.arghavan@gmail.com>
2019-03-19 14:24:06 -0500

    some practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,15 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.print(("سلام"));
         System.out.print((" به دنیا "));
+//        math
+        double sum = 5. / 3;
+        System.out.println(sum);
+//        string
+        String hello = "Hello this is a string";
+        Character character = 'h';
+
+        System.out.println(hello);
+        System.out.println(character);
     }
 
 }
