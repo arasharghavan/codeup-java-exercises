f18887f75443883a7d35fb96a77aa4e47f6c3fba
Arash <arash.arghavan@gmail.com>
2019-03-19 17:04:11 -0500

    practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloWorld {
+
+
     public static void main(String[] args){
+
         System.out.print(("سلام"));
         System.out.print((" به دنیا "));
 //        math
@@ -11,6 +16,62 @@ public class HelloWorld {
 
         System.out.println(hello);
         System.out.println(character);
+
+//        Exercises
+
+        int myFavoriteNumber = 740;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "من استرینگ هستم";
+        System.out.println(myString);
+
+        String myString1 = "3.14159";
+        System.out.println(myString1);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+        float myNumber1 = 3.41F;
+        System.out.println(myNumber1);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y= 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+
+        int b = 3;
+        int c = 4;
+        c *= b;
+        System.out.println(c);
+
+        int e = 10;
+        int f = 2;
+        f -= (e / f);
+        System.out.println(f);
+
+//        -------------------------------
+//        int number = 740;
+//        String last = "Arghavan";
+//        String name = "Arash";
+        //System.out.format("hello, %s %s %s nice to see you!", name, last, number);
+
+//        -----------------------------------
+
+        System.out.println("Enter username:");
+        String UserName = new Scanner(System.in).nextLine();
+        System.out.println("User name is : " + UserName);
+        System.out.format("User name is : %s",UserName);
+
+
     }
 
 }
+
+
