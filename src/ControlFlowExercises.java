867d7ea54c7b43dc868d5b813a2bd40580f5b444
Arash <arash.arghavan@gmail.com>
2019-03-21 11:50:26 -0500

    for loop done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -55,6 +55,19 @@ public class ControlFlowExercises {
 //        } while (i < 1000000);
 
 
+        //for
+
+//        for (long i = 5; i <= 15; i++ ){
+//            System.out.println(i);
+//        }
+
+//        for (long i = 0; i < 100; i += 2){
+//            System.out.println(i);
+//        }
+
+//          for (long i = 2; i < 1000000; i *= i){
+//              System.out.println(i);
+//           }
 
 
 
