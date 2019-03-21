cfd385914de162221b4b3da727d298c4211b6e65
Arash <arash.arghavan@gmail.com>
2019-03-21 11:20:04 -0500

    While loop done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,37 @@
 public class ControlFlowExercises {
+
+    public static void main(String[] args){
+
+//        for (int num = 0; num <= 30; num++) {
+//
+//            if (num % 15 == 0) {
+//                System.out.println("fizzbuzz");
+//            } else if (num % 5 == 0) {
+//                System.out.println("buzz");
+//            } else if (num % 3 == 0) {
+//                System.out.println("fizz");
+//            } else {
+//                System.out.println(num);
+//            }
+//
+//        }
+
+//        char symbol = 'خ';
+//        System.out.println(Character.getNumericValue(symbol));
+
+
+//        While
+
+        int i = 5;
+        while (i <= 15){
+            System.out.println(i);
+            i++;
+        }
+
+
+
+
+
+    }
+
 }
