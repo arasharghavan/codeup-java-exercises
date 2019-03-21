7fccd98ab126d17d8c4b76d4d147ccf16f4201ac
Arash <arash.arghavan@gmail.com>
2019-03-21 11:42:29 -0500

    Do While done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -20,13 +20,42 @@ public class ControlFlowExercises {
 //        System.out.println(Character.getNumericValue(symbol));
 
 
+
+
 //        While
 
-        int i = 5;
-        while (i <= 15){
-            System.out.println(i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+
+        //Do While
+
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i = i + 2;
+//
+//        }while (i < 100);
+//-------------------------------->
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//
+//        }while (i >= -10);
+//----------------------------------->
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i =  i * i;
+//        } while (i < 1000000);
+
+
+
 
 
 
