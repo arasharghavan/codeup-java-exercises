d5c5889f0e49206b61b2afdf11f60137296b6b17
Arash <arash.arghavan@gmail.com>
2019-03-21 11:54:04 -0500

    FizzBuzz done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,19 +2,6 @@ public class ControlFlowExercises {
 
     public static void main(String[] args){
 
-//        for (int num = 0; num <= 30; num++) {
-//
-//            if (num % 15 == 0) {
-//                System.out.println("fizzbuzz");
-//            } else if (num % 5 == 0) {
-//                System.out.println("buzz");
-//            } else if (num % 3 == 0) {
-//                System.out.println("fizz");
-//            } else {
-//                System.out.println(num);
-//            }
-//
-//        }
 
 //        char symbol = 'خ';
 //        System.out.println(Character.getNumericValue(symbol));
@@ -70,7 +57,20 @@ public class ControlFlowExercises {
 //           }
 
 
+        //Fizzbuzz
 
+        for (int num = 0; num <= 100; num++) {
+
+            if (num % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (num % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (num % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(num);
+            }
+        }
 
 
 
