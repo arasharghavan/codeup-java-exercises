ef2082b9a5e437fb7978d3ccee3d484d18460da6
Arash <arash.arghavan@gmail.com>
2019-04-01 20:56:18 -0500

    first commit

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,4 +1,75 @@
 package grades;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 
 public class GradesApplication {
+    public static void main(String[] args) {
+
+        Map<String,Student> students = new HashMap<>();
+
+        Student arash = new Student("arash");
+        arash.addGrade(100);
+        arash.addGrade(90);
+        arash.addGrade(95);
+
+        Student sudi = new Student("sudi");
+        sudi.addGrade(100);
+        sudi.addGrade(30);
+        sudi.addGrade(85);
+
+        Student babak = new Student("babak");
+        babak.addGrade(99);
+        babak.addGrade(10);
+        babak.addGrade(45);
+
+        students.put("gitArash",arash);
+        students.put("gitSudi",sudi);
+        students.put("gitBabak",babak);
+
+
+        System.out.println("Welcome!");
+        System.out.println("Here are the github usernames of our students:");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        for (String x: students.keySet()){
+            System.out.print("|" + x);
+        }
+
+        System.out.println();
+        System.out.println(students.containsKey("pizza"));
+
+        System.out.println("Would you like to see another student?");
+
+
+
+      String input = "sudi";
+
+       for (String x:students.keySet()){
+
+           String val = students.get(x).toString();
+           if (input.equals(x)){
+               System.out.println(val + x);
+           }
+       }
+
+
+    }
+
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,56 @@
 package grades;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Student {
+    private String name;
+    private List<Integer> grades;
+
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double total = 0;
+        for (Integer x:grades){
+            total += x;
+        }
+
+        return total / grades.size();
+    }
+
+
+
+//    for test :
+
+//    public static void main(String[] args) {
+//        Student a = new Student("arash");
+//        a.addGrade(100);
+//        a.addGrade(91);
+//
+//        System.out.println(a.getGradeAverage());
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", grades=" + getGradeAverage() +
+//                '}';
+//    }
 }
